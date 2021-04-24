27f76a9cceedaf5ed6c8af0f0d40a62d75828750
Konrad Bolesławski <42543331+konradusko@users.noreply.github.com>
2021-04-24 20:37:07 +0200

    2.4

--- javaScript.js
+++ javaScript.js
@@ -4,8 +4,8 @@ let isThereComma = false; // czy jest przecinek
 let isthereProcent = false; // czy jest procent
 let calculations = ""; //obliczenia nie widoczne
 let tmpComma = false; // zapamietuje na stałe czy byl przecinek uzyty
-let block = false; // jeśli mamy wynik to blokuje przyciski do póki nie zresetujemy go "C"
-let globalBlockProcent = false;
+let globalBlockProcent = false;//globalnie blokuje procenty
+let afterSum = false; //po zsumowaniu zeby mozna bylo dalej operować
 window.addEventListener('click', (el) => {
     //klikanie myszką
     if (el.target.className == "button" || el.target.className == "button BTNStrongComma" || el.target.className == "StrongComma") {
@@ -34,15 +34,15 @@ window.addEventListener('keydown', (el) => {
 //funkcja akcji
 function operationalSigns(sign) {
     let choosenSign = undefined;
-    if (sign == "+" && bottomNumbers.innerHTML != 0 && block == false) {
+    if (sign == "+" && bottomNumbers.innerHTML != 0 ) {
         choosenSign = " + ";
-    } else if (sign == "-" && bottomNumbers.innerHTML != 0 && block == false) {
+    } else if (sign == "-" && bottomNumbers.innerHTML != 0 ) {
         choosenSign = " - ";
-    } else if (sign == "/" && bottomNumbers.innerHTML != 0 && block == false) {
+    } else if (sign == "/" && bottomNumbers.innerHTML != 0 ) {
         choosenSign = " / ";
-    } else if (sign == "x" && bottomNumbers.innerHTML != 0 && block == false || sign == "X" && bottomNumbers.innerHTML != 0 && block == false || sign == "*" && bottomNumbers.innerHTML != 0 && block == false) {
+    } else if (sign == "x" && bottomNumbers.innerHTML != 0  || sign == "X" && bottomNumbers.innerHTML != 0  || sign == "*" && bottomNumbers.innerHTML != 0 ) {
         choosenSign = " * ";
-    } else if (sign == "%" && bottomNumbers.innerHTML != 0 && block == false && globalBlockProcent == false) {
+    } else if (sign == "%" && bottomNumbers.innerHTML != 0  && globalBlockProcent == false) {
         //dodaje % przy liczbie badź go odejmuje
         if (bottomNumbers.innerHTML.charAt(bottomNumbers.innerHTML.length - 1) != '%' && isthereProcent == false) {
             bottomNumbers.innerHTML = bottomNumbers.innerHTML + "%"
@@ -52,7 +52,7 @@ function operationalSigns(sign) {
             bottomNumbers.innerHTML = bottomNumbers.innerHTML.substring(0, bottomNumbers.innerHTML.length - 1)
             isthereProcent = false;
         }
-    } else if ( sign == "=" && bottomNumbers.innerHTML != "-" && topNumbers.innerHTML.length > 0 && block == false || isthereProcent == true && sign == "=" && bottomNumbers.innerHTML != "0" && bottomNumbers.innerHTML != "-" && block == false) {
+    } else if ( sign == "=" && bottomNumbers.innerHTML != "-" && topNumbers.innerHTML.length > 0  || isthereProcent == true && sign == "=" && bottomNumbers.innerHTML != "0" && bottomNumbers.innerHTML != "-" ) {
         //obliczanie wyniku
         let result;
         if (isthereProcent == true && topNumbers.innerHTML == "") {
@@ -83,8 +83,10 @@ function operationalSigns(sign) {
         }
          //wyświetlam wynik
         bottomNumbers.innerHTML = result;
-        block = true;
-    } else if (sign == "+/-" && block == false) {
+        // block = true;
+        calculations = "";
+        afterSum = true;
+    } else if (sign == "+/-" ) {
         //dodaje lub zabieram znak -
         if (bottomNumbers.innerHTML.length == "1" && bottomNumbers.innerHTML.charAt(0) == '-') {
             bottomNumbers.innerHTML = "0"
@@ -97,7 +99,6 @@ function operationalSigns(sign) {
         }
     } else if (sign == "c" || sign == "C" || sign == "Backspace") {
         //usuwa wszystko
-        block = false;
         calculations = "";
         isthereProcent = false;
         isThereComma = false;
@@ -108,6 +109,10 @@ function operationalSigns(sign) {
     }
     //po wybraniu znaku przenosi liczby na gore i czyści dolna linie
     if (choosenSign != undefined) {
+        if(afterSum == true){
+            afterSum = false;
+            topNumbers.innerHTML ="";
+        }
         if (isthereProcent == true) {
             //obliczam procent odpowiedni i dodaje do obliczeń niewidocznych 
             calculations = calculations.concat(procent() + choosenSign);
@@ -122,33 +127,30 @@ function operationalSigns(sign) {
         tmpComma = false;
     }
 }
-function result(){
-    
-}
 function writeNumber(number) {
     //wpisywanie numerów w dolnej linii
-     if (number == "," && isthereProcent == true && isThereComma == true && block == false && bottomNumbers.innerHTML.charAt(bottomNumbers.innerHTML.length - 1) == ',' && bottomNumbers.innerHTML.charAt(bottomNumbers.innerHTML.length - 2) == '0') {
+     if (number == "," && isthereProcent == true && isThereComma == true && bottomNumbers.innerHTML.charAt(bottomNumbers.innerHTML.length - 1) == ',' && bottomNumbers.innerHTML.charAt(bottomNumbers.innerHTML.length - 2) == '0') {
         isThereComma = false;
         bottomNumbers.innerHTML = bottomNumbers.innerHTML.substring(0, bottomNumbers.innerHTML.length - 2)
         //zabiera przecinek ale tylko jeśli jest za znakiem %
-    } else if (number == "," && isThereComma == false && block == false && bottomNumbers.innerHTML == "-"||number == "," && isThereComma == false && block == false && bottomNumbers.innerHTML.charAt(bottomNumbers.innerHTML.length - 1) == '%') {
+    } else if (number == "," && isThereComma == false  && bottomNumbers.innerHTML == "-"||number == "," && isThereComma == false  && bottomNumbers.innerHTML.charAt(bottomNumbers.innerHTML.length - 1) == '%') {
         isThereComma = true;
         tmpComma = true;
         bottomNumbers.innerHTML = bottomNumbers.innerHTML.concat("0,");
         //dodaje przecinek dokładnie to 0. po % 
         //ew dodaje 0. jak jest sam minus
-    } else if (number == "," && isThereComma == false && block == false) {
+    } else if (number == "," && isThereComma == false ) {
         //dodaje przecinek
         isThereComma = true;
         tmpComma = true;
         bottomNumbers.innerHTML = bottomNumbers.innerHTML.concat(",");
-    } else if (number != "," && bottomNumbers.innerHTML == "0" && block == false) {
+    } else if (number != "," && bottomNumbers.innerHTML == "0" ) {
         bottomNumbers.innerHTML = number.toString();
         //wpisywanie liczby jeśli na początku jest zero
-    } else if (number != "," && bottomNumbers.innerHTML != "0" && block == false) {
+    } else if (number != "," && bottomNumbers.innerHTML != "0" ) {
         //wpisywanie liczb klawiatura/myszka
         bottomNumbers.innerHTML = bottomNumbers.innerHTML.concat(number.toString());
-    } else if (number == "," && bottomNumbers.innerHTML == "0." && block == false && isThereComma == true) {
+    } else if (number == "," && bottomNumbers.innerHTML == "0."  && isThereComma == true) {
         bottomNumbers.innerHTML = "0";
         isThereComma = false;
         //zabiera przecinek gdy mamy zero na dolnej lini
